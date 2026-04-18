b04fccc34727e894c737cd781ce63bfb30065f25
Jyoti Sharma <angel@MacBook-Air.local>
2026-04-18 11:07:41 +0530

    removed  from base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/portfolio/' : '/',
+  base: '/',
   build: { outDir: 'dist' }
 })
